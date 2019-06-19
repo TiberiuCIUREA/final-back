37ac2072d52c480f596683943c7caca810f97674
TIBERIU MIHAI CIUREA <tiberiu.ciurea@ro.ibm.com>
2019-06-19 10:00:58 +0300

    refactor

--- src/main/java/com/tiberiu/mentool/domain/user/ValidUserGenerator.java
+++ src/main/java/com/tiberiu/mentool/domain/user/ValidUserGenerator.java
@@ -3,7 +3,7 @@ package com.tiberiu.mentool.domain.user;
 public class ValidUserGenerator {
 
     public static final User TIBERIU = new User(
-            new EmailAddress("tiberiu@tiberiu.com"), "Tiberiu", "Cucumber warchief",
+            new EmailAddress("tiberiu@tiberiu.com"), "Tiberiu", "Ciurea",
             new PhoneNumber("1234567890")
     );
 }
